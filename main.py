d548013f5c04ba2b14312e86a9796785b5840127
Hadi Barmaki <hadibarmaki75@gmail.com>
2024-07-18 10:44:49 -0700

    style: write docstring

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ class DiscountError(Exception):
 
 
 def apply_discount(price: int, discount: float = 0.0) -> int:
+    """Calculates the Final Price After Discount 
+    """
     final_price = int(price * (1 - discount))
     if not 0 <= final_price < price:
         raise DiscountError('Invalid price')
