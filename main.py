d628e4966b50b47e6b95934480bd7f418d5bc3bf
Hadi Barmaki <hadibarmaki75@gmail.com>
2024-07-18 08:22:52 -0700

    debugging function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def apply_discount(price, discount):
-    final_price = int(pri * (1 - discount))
+    final_price = int(price * (1 - discount))
     if not 0 <= final_price < price:
         raise ValueError('Invalid Discount')
     
