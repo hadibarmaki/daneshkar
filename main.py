77ae97a550bbf294281fcd0e3b31f09bf626e0bb
Hadi Barmaki <hadibarmaki75@gmail.com>
2024-07-18 03:32:56 -0700

    style: write type annotations

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ class DiscountError(Exception):
     pass
 
 
-def apply_discount(price, discount):
+def apply_discount(price: int, discount: float = 0.0) -> int:
     final_price = int(price * (1 - discount))
     if not 0 <= final_price < price:
         raise DiscountError('Invalid price')
