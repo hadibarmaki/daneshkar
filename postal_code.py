542774bed4525e3ee5f9d87b67566b9ed449ab46
Hadi Barmaki <hadibarmaki75@gmail.com>
2024-07-22 18:41:38 -0700

    Revert "feat: write another example"
    This reverts commit 31bb72f2b00c9e24e64a8025a72d0b3a44ca2452.

--- postal_code.py
+++ postal_code.py
@@ -23,9 +23,4 @@ zip_codes = [
     '123.4-56789',
 ]
 
-# print(list(filter(is_valid_code, zip_codes)))
-result = []
-for zip_code in zip_codes:
-    if is_valid_code(zip_code):
-        result.append(zip_code)
-print(result)        
+print(list(filter(is_valid_code, zip_codes)))
